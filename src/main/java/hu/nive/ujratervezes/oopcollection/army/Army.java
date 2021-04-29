9a6ab76896e60fb6150345f48b8d99fd9271dccb
mozes <mozes.bence@gmail.com>
2021-04-29 12:57:17 +0200

    1234

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/Army.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/Army.java
@@ -8,9 +8,23 @@ public class Army {
 
     public void addUnit(MilitaryUnit militaryUnit){units.add(militaryUnit);}
 
-    public void damageAll(int damage){}
+    public void damageAll(int damage){
+        for (int i = 0; i < units.size(); i++) {
+            units.get(i).sufferDamage(damage);
+            if (units.get(i).getHitPoints() < 25){
+                units.remove(i);
+                i--;
+            }
+        }
+    }
 
-    public int getArmyDamage(){return 0;}
+    public int getArmyDamage(){
+        int count = 0;
+        for (int i = 0; i < units.size(); i++) {
+            count += units.get(i).doDamage();
+        }
+        return count;
+    }
 
     public int getArmySize(){return units.size();}
 }
